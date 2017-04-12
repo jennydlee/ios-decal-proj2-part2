ebd096d7d4b0b9e9f8cbecb9d19d68e17e79b0be
jennydlee <jennydlee@berkeley.edu>
2017-04-11 17:34:52 -0700

    done

--- SnapchatClonePt3/CurrentUser.swift
+++ SnapchatClonePt3/CurrentUser.swift
@@ -24,13 +24,6 @@ class CurrentUser {
         id = currentUser?.uid
     }
     
-    /*
-        TODO:
-        
-        Retrieve a list of post ID's that the user has already opened and return them as an array of strings.
-        Note that our database is set up to store a set of ID's under the readPosts node for each user.
-        Make a query to Firebase using the 'observeSingleEvent' function (with 'of' parameter set to .value) and retrieve the snapshot that is returned. If the snapshot exists, store its value as a [String:AnyObject] dictionary and iterate through its keys, appending the value corresponding to that key to postArray each time. Finally, call completion(postArray).
-    */
     func getReadPostIDs(completion: @escaping ([String]) -> Void) {
         var postArray: [String] = []
         dbRef.child(firUsersNode).child(id!).observeSingleEvent(of: .value, with: { (snapshot) in
@@ -42,20 +35,12 @@ class CurrentUser {
                     }
                 }
             }
-
         }) { (error) in
             print(error.localizedDescription)
         }
         completion(postArray)
     }
     
-    /*
-        TODO:
-     
-        Adds a new post ID to the list of post ID's under the user's readPosts node.
-        This should be fairly simple - just create a new child by auto ID under the readPosts node and set its value to the postID (string).
-        Remember to be very careful about following the structure of the User node before writing any data!
-    */
     func addNewReadPost(postID: String) {
         dbRef.child(firUsersNode).child(id!).child(firReadPostsNode).childByAutoId().setValue(postID)
         readPostIDs?.append(postID)
